c1af201fcd18a2d29007b083bc0dfc088d4be6e3
harshey0 <sethiharshitfzr@gmail.com>
2024-04-06 17:19:16 +0530

    login details fetch

--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -1,8 +1,19 @@
 import "../styles/signin.css";
 import {Container , Box , TextField,Link as MuiLink, Button, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
+import { useState } from "react";
 
 export default function Signin() {
+
+const [ username , newusername] = useState("");
+const [ password , newpassword] = useState("");
+
+function handle()
+{
+  console.log(username);
+  console.log(password);
+}
+
   return (
    <Container className="login-container" >
    <Box className="login-box">
@@ -17,7 +28,9 @@ export default function Signin() {
               fullWidth
               type="username"
               label="Username"
-              id="username"/>
+              id="username"
+                onChange={(e)=>newusername(e.target.value)}
+              />
     <TextField 
               margin="normal" 
               variant="outlined"
@@ -25,12 +38,15 @@ export default function Signin() {
               fullWidth
               type="password"
               label="Password"
-              id="password"/>
+              id="password"
+              onChange={(e)=>newpassword(e.target.value)}
+              />
 
               <MuiLink component={Link} to="/" className="login-pad">Forgot Password?Click here</MuiLink>
 
               <Button className="login-pad"
-                variant="contained" style={{backgroundColor:'red'}}>Sign in</Button>
+                variant="contained" style={{backgroundColor:'red'}}
+                onClick={handle}>Sign in</Button>
               <a href="#" style={{ textDecoration: 'none' }}>
                     <Button className="login-pad" variant="contained" style={{ backgroundColor: 'blue' }}>
                                      Sign in with Google
