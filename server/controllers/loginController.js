61e989a596c42b631b48b53bf60fcf1c77b6421d
harshey0 <sethiharshitfzr@gmail.com>
2024-04-16 23:53:47 +0530

    checkpoint10

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -22,8 +22,6 @@ export default async function loginUser(req,res)
                 
                 if(passwordMatch)
                {
-                res.setHeader('Access-Control-Allow-Origin', 'https://reyy-s-shoes.vercel.app');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
                 generateToken(res,user.username,user.isAdmin);
 
                 return res.send("Login Successful");
@@ -42,8 +40,6 @@ export default async function loginUser(req,res)
 
 export function logoutUser(req,res)
 {
-    res.setHeader('Access-Control-Allow-Origin', 'https://reyy-s-shoes.vercel.app');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
     destroyToken(res);
     return res.send("done");
 }
