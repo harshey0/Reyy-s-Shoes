7dbaf69f1ab88717f7ef6c4945af5e1492c5643f
harshey0 <sethiharshitfzr@gmail.com>
2024-04-17 01:27:58 +0530

    checkpoint14

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT =process.env.PORT;
 
 const app = express ();
 
-app.use(cookieParser);
+app.use(cookieParser());
 // passport(app);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
